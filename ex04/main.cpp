daf039a11396fc595c4494879fe2ed9d4ea5aa71
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-05-14 15:23:28 +0200

    ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 21:07:59 by aelaaser          #+#    #+#             */
-/*   Updated: 2025/05/13 21:31:56 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/05/14 15:22:31 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,36 @@ static int open_files(std::string input_filename, std::string output_filename, s
 	return (0);
 }
 
+void search_replace(std::string find, std::string replace, std::ifstream *inputfile, std::ofstream *outputfile)
+{
+	std::string line;
+	std::string final_line;
+	std::string::size_type	found;
+	int last_find_pos;
+
+	while (std::getline(*inputfile, line))
+	{
+		final_line.clear();
+		last_find_pos = 0;
+		found = line.find(find);
+		if (found == std::string::npos)
+			final_line = line;
+		while (found != std::string::npos)
+		{
+			final_line.append(line, last_find_pos, (found - last_find_pos));
+			final_line += replace;
+			last_find_pos = found + find.length();
+			found = line.find(find, last_find_pos);
+			if (found == std::string::npos)
+				final_line.append(line, last_find_pos, line.length());
+		}
+		if ((*inputfile).eof())
+			(*outputfile) << final_line;
+		else
+			(*outputfile) << final_line << std::endl;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	std::string input_filename;
@@ -48,6 +78,7 @@ int main(int argc, char **argv)
 	output_filename = input_filename + ".replace";
 	if (open_files(input_filename, output_filename, &inputfile, &outputfile))
 		return (1);
+	search_replace(argv[2], argv[3], &inputfile, &outputfile);
 	inputfile.close();
 	outputfile.close();
 	return (0);
