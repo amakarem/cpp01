a648e026775bfe567553335a7a499b1cce110ae9
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-05-14 16:14:57 +0200

    ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 15:41:40 by aelaaser          #+#    #+#             */
-/*   Updated: 2025/05/14 15:59:36 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/05/14 16:11:29 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ void	Harl::debug(void)
 }
 void	Harl::info(void)
 {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+	std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
 void	Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n";
+	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years, whereas you started working here just last month.\n";
 }
 void	Harl::error(void)
 {
